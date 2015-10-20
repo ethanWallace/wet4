9add531adc45f28e97f638dc368055f7df52ab87
Steph4104 <stephanie4104@gmail.com>
2015-10-20 09:15:29 -0400

    Remove archive
    In blog sidebar, I remove archive section

--- views/default/blog/sidebar.php
+++ views/default/blog/sidebar.php
@@ -12,12 +12,12 @@ if ($vars['page'] != 'friends') {
 		'container_guid' => elgg_get_page_owner_guid(),
 	));
 }
-
+/*
 // only users can have archives at present
 if ($vars['page'] == 'owner' || $vars['page'] == 'group') {
 	echo elgg_view('blog/sidebar/archives', $vars);
 }
-
+*/
 if ($vars['page'] != 'friends') {
 	echo elgg_view('page/elements/tagcloud_block', array(
 		'subtypes' => 'blog',
