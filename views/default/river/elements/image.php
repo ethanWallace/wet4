eef1f6f586d489886b73fdcbf10b80c0d6a20102
piet0024 <n.pietrantonio0024@live.com>
2015-10-30 16:29:49 -0400

    Activity River and widget image changes
    small change to the activity river in widget view

--- views/default/river/elements/image.php
+++ views/default/river/elements/image.php
@@ -11,9 +11,17 @@ $item = $vars['item'];
 /* @var ElggRiverItem $item */
 
 $subject = $item->getSubjectEntity();
-
-if (elgg_in_context('widgets')) {
+if(elgg_get_context() !== 'widgets'){
+  if (elgg_in_context('widgets')) {
 	echo elgg_view_entity_icon($subject, 'medium');
 } else {
 	echo elgg_view_entity_icon($subject, 'medium');
+}  
+}else{
+ if (elgg_in_context('widgets')) {
+	echo elgg_view_entity_icon($subject, 'small');
+} else {
+	echo elgg_view_entity_icon($subject, 'small');
+}   
 }
+
